e00b8ddd4d8d8553fd4eb6d180f3f6cc2227af12
lewis karanu <lewiskaranu0@gmail.com>
2024-04-16 14:18:03 +0300

    aaded

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("task-list");
+
+  form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      const taskInput = document.getElementById("new-task-description");
+      const taskDescription = taskInput.value;
+      
+      if (taskDescription.trim() !== "") {
+          const taskItem = document.createElement("li");
+          taskItem.textContent = taskDescription;
+          taskList.appendChild(taskItem);
+          taskInput.value = "";
+      } else {
+          alert("Please enter a task description.");
+      }
+  });
 });
